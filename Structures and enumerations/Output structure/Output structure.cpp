c39da485d474b0f3e22a49275ab058965e2f6cd7
Vitaly <malyshewws@gmail.com>
2024-05-02 21:00:55 +0300

    Initial commit

---
+++ Structures and enumerations/Output structure/Output structure.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <string>
+#include<Windows.h>
+
+    struct Address 
+    {
+        std::string city;
+        std::string street;
+        int houseNumber;
+        int apartmentNumber;
+        std::string zipCode;
+    };
+
+    void printAddress(const Address & addr) 
+    {
+        std::cout << "Город: " << addr.city << std::endl;
+        std::cout << "Улица: " << addr.street << std::endl;
+        std::cout << "Номер дома: " << addr.houseNumber << std::endl;
+        std::cout << "Номер квартиры: " << addr.apartmentNumber << std::endl;
+        std::cout << "Индекс: " << addr.zipCode << std::endl;
+    }
+
+    int main(int argc, char** argv) 
+    {
+        setlocale(LC_ALL, "rus");
+
+        SetConsoleCP(1251);
+
+
+        Address addr1, addr2;
+        std::cout << " Введите город: ";
+        std::cin >> addr1.city;
+        std::cout << " Введите улицу: ";
+        std::cin >> addr1.street;
+        std::cout << " Введите номер дома: ";
+        std::cin >> addr1.houseNumber;
+        std::cout << " Введите номер квартиры: ";
+        std::cin >> addr1.apartmentNumber;
+        std::cout << " Введите индекс: ";
+        std::cin >> addr1.zipCode;
+
+        printAddress(addr1);
+
+        return 0;
+    }
+
+/*Создайте структуру для хранения адреса. Структура должна содержать название города, улицы, номер дома, номер квартиры и индекс.
+
+Создайте функцию для вывода структуры на консоль.
+
+Протестируйте созданную функцию. Создайте в коде экземпляры структуры, передайте их в функцию и выведите на консоль получившиеся экземпляры структуры.
+
+Пример работы программы
+Город: Москва
+Улица: Арбат
+Номер дома: 12
+Номер квартиры: 8
+Индекс: 123456
+
+Город: Ижевск
+Улица: Пушкина
+Номер дома: 59
+Номер квартиры: 143
+Индекс: 953769
+
+Структура объявляется вне функций.
+
+Чтобы создать экземпляр структуры, нужно указать её тип.
+
+Для доступа к полям структуры используйте оператор ..
+
+Для ввода значений с консоли используйте std::cin.
+
+Для вывода на консоль используйте std::cout.*/
---
+++ Structures and enumerations/check/check.cpp
@@ -0,0 +1,73 @@
+﻿
+#include <iostream>
+#include <string>
+#include <fstream>
+#include<Windows.h>
+
+
+struct BankAccount            
+ {
+    int accountNumber;               // номер счёта
+    std::string ownerName;          // имя владельца 
+    double balance;                // баланс счёта
+
+ };
+
+ void changeBalance(BankAccount &account, double newBalance)
+ {
+    account.balance = newBalance;
+ }
+
+int main(int argc, char** argv)
+
+{
+    setlocale(LC_ALL, "rus");
+
+    SetConsoleCP(1251);
+   
+    BankAccount account;
+    std::cout << "Введите номер счёта: ";
+
+    std::cin >> account.accountNumber;
+
+    std::cout << "Введите имя владельца: ";
+
+    std::cin >> account.ownerName;
+
+    std::cout << "Введите баланс: ";
+
+    std::cin >> account.balance;
+
+    double newBalance;
+
+    std::cout << "Введите новый баланс: ";
+
+    std::cin >> newBalance;
+
+    changeBalance(account, newBalance);
+
+    std::cout << "Ваш счёт: " << account.ownerName << ", " << account.accountNumber << ", " << account.balance << std::endl;
+
+    return 0;
+}
+
+/*Создайте структуру для хранения информации о банковском счёте. Она должна хранить номер счёта (целое число), имя владельца (строка) и количество денег на счету (дробное число).
+
+Создайте функцию для изменения баланса счёта. Функция должна принимать экземпляр структуры банковского счёта и новую сумму.
+
+Попросите пользователя заполнить поля экземпляра структуры, сообщить новый баланс счёта и выведите обновлённый экземпляр структуры на консоль.
+
+Пример работы программы
+Введите номер счёта: 123456789
+Введите имя владельца: Андрей
+Введите баланс: 5000
+Введите новый баланс: 6000
+Ваш счёт: Андрей, 123456789, 6000
+
+Не забудьте, что для изменения экземпляра структуры внутри функции нужно передавать его по ссылке или через указатель.
+
+Для объявления структуры используйте ключевое слово struct.
+
+Для ввода значений с консоли используйте std::cin.
+
+Для вывода на консоль используйте std::cout.*/
